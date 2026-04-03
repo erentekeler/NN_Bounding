5c0ebe66c4f488da6bba5efdb8ddcf3ebdc1fffa
Eren Tekeler <ee.erentekeler@gmail.com>
2026-04-02 23:31:50 -0400

    Forward has three relaxation methods implemented: forward, IBP, and custom.

--- src/bound_prop/forward_lirpa.py
+++ src/bound_prop/forward_lirpa.py
@@ -4,34 +4,54 @@ from typing import Dict
 
 from src.NN_model import NeuralNetwork
 from src.Bounding import Bounding
+from src.bound_prop.IBP import IBP
 import numpy as np
 
 
 
-class forward_lirpa():
+class forward_lirpa(Bounding):
     def __init__(self, model, input_range=None,
                 eps=None, x_0=None, norm=None, c=None,
-                mode="forward+IBP", # forward
+                relaxation_method="IBP", # IBP, forward, custom
+                compute_interm_bounds=True, # If true, all intermediate bounds are computed and stored. Only for IBP and custom relaxation methods
                 ub_relaxations: Dict =None,
                 lb_relaxations: Dict =None):
-        self.device = 'cuda' if torch.cuda.is_available() else "cpu"
 
-        bound_computer = Bounding(model, input_range=input_range, eps=eps, x_0=x_0, norm=norm)
-        self.layer_information = bound_computer.layer_information 
-        self.set_relaxations(ub_relaxations, lb_relaxations) # This function is called to set user defined relaxations, if any given
-
+        # Initializing the class attributes
         self.model = model
+        self.device = 'cuda' if torch.cuda.is_available() else "cpu"
         self.c = c.to(self.device) if c is not None else c # Specification vector
 
         # put all input related parameters in a dictionary, this is later processed to find the dual norm and simplify concretization
         self.input_specs = {"input_range": input_range, "eps": eps, "x_0": x_0, "norm": norm}
         self.process_input_specs()
 
-        self.mode = mode # Getting the forward bound prop mode, could be forward or forward+IBP
+        '''Three options for getting the relaxation paramaters, IBP, forward and custom.'''
+        self.relaxation_method = relaxation_method
+        if relaxation_method == "IBP": # If relaxations are IBP based, compute the relaxation parameters
+            IBP_temp = IBP(model, input_range=input_range, eps=eps, x_0=x_0, norm=norm, c=c, compute_relaxation_params=True)
+            IBP_temp.compute_bounds(print_out_bounds=False, print_interm_bounds=False)
+            if compute_interm_bounds: # it is possible to compute the intermediate bounds or directly the output bounds
+                IBP_temp.add_interm_bound_columns(bound_prop_method="forward")
+            self.layer_information = IBP_temp.layer_information
+        
+        elif relaxation_method == "forward":
+            # initializing the bounding object, layer_information dataframe comes from here
+            # If pure forward, then relaxation parameter and intermediate bound computation is needed.
+            super().__init__(model=model, method=relaxation_method, compute_relaxation_params=True, compute_interm_bounds=True)  
+        
+        elif relaxation_method == "custom":
+            # initializing the bounding object, layer_information dataframe comes from here 
+            super().__init__(model=model, method=relaxation_method, compute_relaxation_params=True)
+            if compute_interm_bounds: # it is possible to compute the intermediate bounds or directly the output bound
+                self.add_interm_bound_columns(bound_prop_method="forward")
+            self.set_relaxations(ub_relaxations, lb_relaxations) # This function is called to set user defined relaxations
+
+        self.compute_interm_bounds = compute_interm_bounds
 
         
     def set_relaxations(self, ub_relaxations, lb_relaxations):
-        '''If user passes specific relaxation parameters rather than IBP precomputed bounds, the values are set here'''
+        '''If user passes specific relaxation parameters, the values are set here'''
         if ub_relaxations is not None:
             for key, value in ub_relaxations.items():
                 for column, params in value.items():
@@ -48,7 +68,7 @@ class forward_lirpa():
         return self.model[0].weight.shape[1] # Getting the number of input features of the NN
 
 
-    def compute_A_d(self, bound_interm=False, bound_only_out=True):
+    def compute_A_d(self, model, layer_information):
         '''In this function the forward lirpa is performed and the A matrix along with the d vector is returned for the upper and lower bounds'''
         # Getting the number of input features
         A_init_dim = self.in_features()
@@ -62,38 +82,38 @@ class forward_lirpa():
 
         A_ub_temp, A_lb_temp = None, None # These are to keep the diag(alpha_ub)A_ub and diag(alpha_lb)A_lb terms
 
-        for layer_idx, layer in self.layer_information.iterrows():
+        for layer_idx, layer in layer_information.iterrows():
             if layer_idx == 0:
                 # Bias vector
-                d_ub = self.model[layer_idx].bias
-                d_lb = self.model[layer_idx].bias
+                d_ub = model[layer_idx].bias
+                d_lb = model[layer_idx].bias
 
                 # A matrix
-                A_ub = self.model[layer_idx].weight@A_ub
-                A_lb = self.model[layer_idx].weight@A_lb
+                A_ub = model[layer_idx].weight@A_ub
+                A_lb = model[layer_idx].weight@A_lb
             else:
                 if layer["Layer_type"] == "nn.Linear":
-                    zero_W = torch.zeros_like(self.model[layer_idx].weight) # Creating the comparison matrix to get the positive and negative elements of A
+                    zero_W = torch.zeros_like(model[layer_idx].weight) # Creating the comparison matrix to get the positive and negative elements of A
 
                     # A matrix
                     A_ub_temp = A_ub.clone()
                     A_lb_temp = A_lb.clone()
-                    A_ub = torch.max(zero_W, self.model[layer_idx].weight)@A_ub_temp + torch.min(zero_W, self.model[layer_idx].weight)@A_lb_temp
-                    A_lb = torch.max(zero_W, self.model[layer_idx].weight)@A_lb_temp + torch.min(zero_W, self.model[layer_idx].weight)@A_ub_temp
+                    A_ub = torch.max(zero_W, model[layer_idx].weight)@A_ub_temp + torch.min(zero_W, model[layer_idx].weight)@A_lb_temp
+                    A_lb = torch.max(zero_W, model[layer_idx].weight)@A_lb_temp + torch.min(zero_W, model[layer_idx].weight)@A_ub_temp
 
                     # Bias vector
                     d_ub_temp = d_ub.clone()
                     d_lb_temp = d_lb.clone()
-                    d_ub = torch.max(zero_W, self.model[layer_idx].weight)@d_ub_temp + torch.min(zero_W, self.model[layer_idx].weight)@d_lb_temp + self.model[layer_idx].bias
-                    d_lb = torch.max(zero_W, self.model[layer_idx].weight)@d_lb_temp + torch.min(zero_W, self.model[layer_idx].weight)@d_ub_temp + self.model[layer_idx].bias
+                    d_ub = torch.max(zero_W, model[layer_idx].weight)@d_ub_temp + torch.min(zero_W, model[layer_idx].weight)@d_lb_temp + model[layer_idx].bias
+                    d_lb = torch.max(zero_W, model[layer_idx].weight)@d_lb_temp + torch.min(zero_W, model[layer_idx].weight)@d_ub_temp + model[layer_idx].bias
 
                 elif layer["Layer_type"] == "nn.ReLU":
                     # Getting the slopes and biases of the convex relaxation of the activation function and diagonalizing them
-                    slope_upper = torch.diag(self.layer_information.loc[layer_idx, "Upper_bound_slope"]).to(self.device)
-                    bias_upper = self.layer_information.loc[layer_idx, "Upper_bound_bias"].to(self.device)
+                    slope_upper = torch.diag(layer_information.loc[layer_idx, f"{self.relaxation_method}_ub_slope"]).to(self.device)
+                    bias_upper = layer_information.loc[layer_idx, f"{self.relaxation_method}_ub_bias"].to(self.device)
 
-                    slope_lower = torch.diag(self.layer_information.loc[layer_idx, "Lower_bound_slope"]).to(self.device)
-                    bias_lower = self.layer_information.loc[layer_idx, "Lower_bound_bias"].to(self.device)
+                    slope_lower = torch.diag(layer_information.loc[layer_idx, f"{self.relaxation_method}_lb_slope"]).to(self.device)
+                    bias_lower = layer_information.loc[layer_idx, f"{self.relaxation_method}_lb_bias"].to(self.device)
 
                     # A matrix
                     A_ub = slope_upper@A_ub
@@ -118,10 +138,10 @@ class forward_lirpa():
                 self.input_specs["dual_norm"] = self.input_specs["norm"]/(self.input_specs["norm"]-1) # From 1/p + 1/q = 1
 
 
-    def compute_bounds(self, print_out_bounds=True):
+    def concretize_bounds(self, output_bound_params, c, print_out_bounds):
         '''This method computes the bounds via concretization and print&return them.'''
         # First we compute the relaxation matrix and vectors
-        A_ub, A_lb, d_lb, d_ub = self.compute_A_d()
+        A_ub, A_lb, d_lb, d_ub = output_bound_params # unpacking (A_ub, A_lb, d_lb, d_ub)
         A_ub_temp, A_lb_temp, d_lb_temp, d_ub_temp = A_ub.clone(), A_lb.clone(), d_lb.clone(), d_ub.clone()
 
         # if input range is not given, so it is a pure norm ball, then directly apply the dual norm
@@ -129,13 +149,13 @@ class forward_lirpa():
             # min (a_i^Tx + d) over ||x-x_0||_p <= epsilon    =>   -epsilon||a_i||_q + a_i^T x_0 + d
             # max (a_i^Tx + d) over ||x-x_0||_p <= epsilon    =>   epsilon||a_i||_q + a_i^T x_0 + d, beautiful!
 
-            if self.c is None: # I'll make this part more efficient, my idea is to make c an I matrix if it is given None.
+            if c is None: # I'll make this part more efficient, my idea is to make c an I matrix if it is given None.
                 lb = -self.input_specs["eps"]*torch.norm(A_lb, p=self.input_specs["dual_norm"], dim=1) + torch.matmul(A_lb, self.input_specs["x_0"]) + d_lb
                 ub = self.input_specs["eps"]*torch.norm(A_ub, p=self.input_specs["dual_norm"], dim=1) + torch.matmul(A_ub, self.input_specs["x_0"]) + d_ub
             else:
                 # getting the diagonal c to compute new A_lb, A_ub, d_lb and d_ub.
                 # if c is positive inequality is preserved else flipped
-                c_diag = torch.diag(self.c.flatten())
+                c_diag = torch.diag(c.flatten())
                 c_zeros = torch.zeros_like(c_diag)
 
                 A_lb = torch.max(c_zeros, c_diag)@A_lb_temp + torch.min(c_zeros, c_diag)@A_ub_temp
@@ -144,7 +164,7 @@ class forward_lirpa():
                 d_lb = torch.max(c_zeros, c_diag)@d_lb_temp + torch.min(c_zeros, c_diag)@d_ub_temp
                 d_ub = torch.max(c_zeros, c_diag)@d_ub_temp + torch.min(c_zeros, c_diag)@d_lb_temp
 
-                sum_vector = torch.ones_like(self.c)
+                sum_vector = torch.ones_like(c)
                 lb = -self.input_specs["eps"]*torch.norm(sum_vector.T@A_lb, p=self.input_specs["dual_norm"]) + torch.matmul(sum_vector.T @ A_lb, self.input_specs["x_0"]) + sum_vector.T@d_lb
                 ub = self.input_specs["eps"]*torch.norm(sum_vector.T@A_ub, p=self.input_specs["dual_norm"]) + torch.matmul(sum_vector.T @ A_ub, self.input_specs["x_0"]) + sum_vector.T@d_ub
         else:
@@ -152,14 +172,14 @@ class forward_lirpa():
             x_sig = (1/2)*(input_range[:, 1] - input_range[:, 0])
             x_mu = (1/2)*(input_range[:, 1] + input_range[:, 0])
 
-            if self.c is None:
+            if c is None:
                 # elementwise computation of the bounds 
                 ub = torch.abs(A_ub)@x_sig + A_ub@x_mu + d_ub
                 lb = -torch.abs(A_lb)@x_sig + A_lb@x_mu + d_lb
             else:
                 # getting the diagonal c to compute new A_lb, A_ub, d_lb and d_ub.
                 # if c is positive inequality is preserved else flipped
-                c_diag = torch.diag(self.c.flatten())
+                c_diag = torch.diag(c.flatten())
                 c_zeros = torch.zeros_like(c_diag)
                 
                 A_lb = torch.max(c_zeros, c_diag)@A_lb_temp + torch.min(c_zeros, c_diag)@A_ub_temp
@@ -168,20 +188,81 @@ class forward_lirpa():
                 d_lb = torch.max(c_zeros, c_diag)@d_lb_temp + torch.min(c_zeros, c_diag)@d_ub_temp
                 d_ub = torch.max(c_zeros, c_diag)@d_ub_temp + torch.min(c_zeros, c_diag)@d_lb_temp
 
-                sum_vector = torch.ones_like(self.c)
+                sum_vector = torch.ones_like(c)
                 ub = torch.abs(sum_vector.T@A_ub)@x_sig + sum_vector.T@A_ub@x_mu + sum_vector.T@d_ub
                 lb = -torch.abs(sum_vector.T@A_lb)@x_sig + sum_vector.T@A_lb@x_mu + sum_vector.T@d_lb
 
         if print_out_bounds:
             self.print_forward_results(lb=lb, ub=ub)
 
-        return (lb, ub)
+        return torch.cat([lb.unsqueeze(1), ub.unsqueeze(1)], dim=1)
+    
+
+    def compute_intermediate_bounds(self, print_out_bounds):
+        '''This function computes the bounds on the partitions of the NN.
+           By doing so layer by layer, intermediate bounds are computed.
+           If relaxation mode is forward, it also computes forward mode based relaxation parameters.'''
+        layer_output_bounds = None 
+        for layer_idx, layer in enumerate(self.model): # going over the layers
+            is_last = (layer_idx == len(self.model)-1) # check for the last layer
+            print_bounds = print_out_bounds and is_last # if it is the last layer, then print the bounds
+            if isinstance(layer, nn.Linear): 
+                # Assigning the layer input bounds and saving them into the layer information df
+                layer_input_bounds = layer_output_bounds
+                self.layer_information.at[layer_idx, 'forward_input_bounds'] = layer_input_bounds.detach().cpu() if layer_input_bounds is not None else None
+
+                # Get the NN from the input to the current layer
+                model = self.model[:layer_idx+1] 
+                layer_information = self.layer_information[:layer_idx+1]
+                
+                # Then compute the output bound params and concretize bounds up until the current layer
+                output_bound_params = self.compute_A_d(model=model, layer_information=layer_information)
+                layer_output_bounds = self.concretize_bounds(output_bound_params=output_bound_params, c=self.c if is_last else None, print_out_bounds=print_bounds)
+                
+                # Storing the bounds in the layer_information df
+                self.layer_information.at[layer_idx, 'forward_output_bounds'] = layer_output_bounds.detach().cpu()
+            
+            elif isinstance(layer, nn.ReLU):
+                # Assigning the layer input bounds and saving them into the layer information df
+                layer_input_bounds = layer_output_bounds
+                self.layer_information.at[layer_idx, 'forward_input_bounds'] = layer_input_bounds.detach().cpu()
+
+                # If relaxation method is forward, then relaxation parameters are computed
+                if self.relaxation_method=="forward":    
+                    self.compute_relaxations(pre_activation_bounds_x=layer_input_bounds, layer_idx=layer_idx)
+
+                # Get the NN from the input to the current layer
+                model = self.model[:layer_idx+1] 
+                layer_information = self.layer_information[:layer_idx+1]
+
+                # Then compute the output bound params and concretize bounds up until the current layer
+                output_bound_params = self.compute_A_d(model=model, layer_information=layer_information)
+                layer_output_bounds = self.concretize_bounds(output_bound_params=output_bound_params, c=self.c if is_last else None, print_out_bounds=print_bounds)
+                
+                # Storing the bounds in the layer_information df
+                self.layer_information.at[layer_idx, 'forward_output_bounds'] = layer_output_bounds.detach().cpu()
+
+        return layer_output_bounds # last computed layer bound = output bound
+            
+
+    def compute_bounds(self, print_out_bounds=True):
+        if (self.relaxation_method=="IBP") or (self.relaxation_method=="custom"):
+            if self.compute_interm_bounds:
+                output_bounds = self.compute_intermediate_bounds(print_out_bounds=print_out_bounds)
+                return output_bounds[:, 0], output_bounds[:, 1] 
+            else:
+                output_bound_params = self.compute_A_d(model=self.model, layer_information=self.layer_information, c=self.c)
+                output_bounds = self.concretize_bounds(output_bound_params=output_bound_params, print_out_bounds=print_out_bounds)
+                return output_bounds[:, 0], output_bounds[:, 1] 
+        elif self.relaxation_method == "forward": # must compute the intermediate bounds
+            output_bounds = self.compute_intermediate_bounds(print_out_bounds=print_out_bounds)
+            return output_bounds[:, 0], output_bounds[:, 1] 
 
             
     def print_forward_results(self, lb, ub):
         # Print the output nicely :) 
         print('\n', '************************************************************************')
-        print(f'{self.mode} Output Bounds: ')
+        print(f'Forward Output Bounds with {self.relaxation_method} relaxations: ')
         if self.c is None:
             for idx, (b_lb, b_ub) in enumerate(zip(lb,ub)):
                 print(f'{b_lb} <= f_{idx}(x) <= {b_ub}')
@@ -219,7 +300,7 @@ if __name__ == "__main__":
     input_size = model[0].weight.shape[1]
     input = torch.cat([torch.rand(input_size).unsqueeze(1)*0.5, 0.5*torch.rand(input_size).unsqueeze(1) + 0.5], dim=1).to(device)
 
-    forward_lirpa = forward_lirpa(model=model, input_range=input, c=None, mode="forward+IBP")
+    forward_lirpa = forward_lirpa(model=model, input_range=input, c=None, relaxation_method="IBP", compute_interm_bounds=True)
     forward_lb, forward_ub = forward_lirpa.compute_bounds(print_out_bounds=True)
 
 
