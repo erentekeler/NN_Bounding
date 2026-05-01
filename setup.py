2d520a58149456198b17e89fdb863a3fd66c2439
Eren Tekeler <ee.erentekeler@gmail.com>
2026-05-01 15:57:56 -0400

    Setup file is added for package installations.

--- setup.py
+++ setup.py
@@ -3,5 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="nn_bounding",
     version="0.1.0",
-    packages=find_packages(),
-)
+    install_requires = ["h5py==3.16.0",
+                    "matplotlib==3.10.8",
+                    "numpy==2.4.4",
+                    "openpyxl==3.1.5",
+                    "pandas==3.0.2",
+                    "seaborn==0.13.2",
+                    "torch==2.8.0",
+                    "gurobipy==13.0.0",
+                    "onnx==1.17.0",
+                    "scipy==1.15.2"],
+    packages=find_packages())
